ac09bb6ffb4017b10a248bac768da394091c27bb
Lukasa <luka@picktek.com>
2012-03-23 19:17:36 +0400

    some function added

--- magitfun_app.js
+++ magitfun_app.js
@@ -1,4 +1,48 @@
 $(function () {
+    var magtifunobj;
+    var transitions = new Array();
+    transitions.push("pop");
+    transitions.push("flip");
+    transitions.push("slide");
+    transitions.push("slideup");
+    transitions.push("slidedown");
+
+    $("#login").click(function (e) {
+        e.preventDefault();
+        var user = $("#username").val();
+        var password = $("#password").val();
+        magtifunobj = new magitfun(user, password);
+        magtifunobj.login(function (a) {
+            if (a == "succses") {
+                $.mobile.changePage($("#pageHome"), {transition:getRandTransition()})
+            } else {
+                navigator.notification.vibrate(250);
+                navigator.notification.alert("Wrong Username or Password", null, "MissBehaive", "I'm Sorry!");
+            }
+        })
+    });
+    $("#send").click(function (e) {
+        e.preventDefault();
+        var recipient = $("#recipients").val();
+        var msgbody = $("#msgBody").val();
+        magtifunobj.send(recipient,msgbody,function (a) {
+            //Nothing Yet
+        })
+    });
+
+    $("#logout").click(function (e) {
+        e.preventDefault();
+        var transition = getRandTransition();
+        console.log(transition);
+        $.mobile.changePage($("#pageLogin"), {transition:transition})
+    });
+    $("#about").click(function (e) {
+        e.preventDefault();
+        var transition = getRandTransition();
+        console.log(transition);
+        $.mobile.changePage($("#pageHome"), {transition:transition})
+    });
+
     $("#navbarSms").click(function (e) {
         e.preventDefault();
         $(".content-sms").css("display", "block");
@@ -17,4 +61,11 @@ $(function () {
         $(".content-sms").css("display", "none");
         $(".content-history").css("display", "none");
     });
+    function getRandTransition() {
+        return transitions[getRandomArbitrary(0, transitions.length)];
+    }
+
+    function getRandomArbitrary(min, max) {
+        return Math.round(Math.random() * (max - min) + min);
+    }
 });
