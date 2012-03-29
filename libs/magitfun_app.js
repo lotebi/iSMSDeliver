d912fd92696ca0d4ca1b07c26f164b35c03a7814
lukasa <mutanta_warlock@yahoo.com>
2012-03-29 21:46:38 +0400

    theading (performance stuff)

--- libs/magitfun_app.js
+++ libs/magitfun_app.js
@@ -19,9 +19,11 @@ $(function () {
             connectorObject.login(function (a) {
                 if (a == "succses") {
                     $.mobile.changePage($("#pageHome"), {transition:getRandTransition()});
-                    refreshBalance();
-                    grabContacts();
-                    grabHistory(2);
+                    window.setInterval(function () {
+                        refreshBalance();
+                        grabContacts();
+                        grabHistory(2);
+                    },500);
                 } else {
                     navigator.notification.vibrate(250);
                     navigator.notification.alert("Wrong Username or Password", null, "MissBehaive", "I'm Sorry!");
