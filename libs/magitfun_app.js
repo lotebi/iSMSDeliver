7700811326eb5bd957480d4fc282d1d08ab5e74e
Lukasa <luka@picktek.com>
2012-04-04 11:31:27 +0400

    small fixes

--- libs/magitfun_app.js
+++ libs/magitfun_app.js
@@ -19,6 +19,7 @@ $(function () {
                 } else {
                     navigator.notification.vibrate(250);
                     navigator.notification.alert("Wrong Username or Password", null, "MissBehaive", "I'm Sorry!");
+                    $.mobile.hidePageLoadingMsg();
                 }
             })
         });
@@ -48,6 +49,7 @@ $(function () {
                     $("#msgBody").val("");
                     refreshBalance();
                     grabHistory(2);
+                    onMsgChange();
                     //connectorObject.getBalance(updateCreditsGel);
                 })
             } else {
